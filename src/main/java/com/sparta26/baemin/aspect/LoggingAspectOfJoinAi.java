b37eb767a218730c5112dcd41b9a1bcdcdef6011
DESKTOP-N8PDVAS\WeSeongGu <fkskdldh@naver.com>
2024-08-25 20:49:21 +0900

    AOP 로그 작성 클래스 생성

---
+++ src/main/java/com/sparta26/baemin/aspect/LoggingAspectOfJoinAi.java
@@ -0,0 +1,14 @@
+package com.sparta26.baemin.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * Ai 관련 로그 작성하는 곳
+ */
+@Aspect
+@Component
+@Slf4j(topic = "AITopic")
+public class LoggingAspectOfJoinAi {
+}
