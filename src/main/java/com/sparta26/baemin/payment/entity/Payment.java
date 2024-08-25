f9d3d4f74bf6cb50d61970453df20a5aa3e04f45
TrendFollow <160304124+TrendFollow@users.noreply.github.com>
2024-08-25 12:05:27 +0900

    feat Payment 엔티티

---
+++ src/main/java/com/sparta26/baemin/payment/entity/Payment.java
@@ -0,0 +1,41 @@
+package com.sparta26.baemin.payment.entity;
+
+import com.sparta26.baemin.common.AuditEntity;
+import com.sparta26.baemin.order.entity.Order;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "p_PAYMENTS")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Payment extends AuditEntity {
+
+    @Id
+    @GeneratedValue
+    @UuidGenerator
+    @Column(name = "payment_id")
+    private UUID id;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus status; // COMPLETE, CANCEL
+
+    @Column(name = "card_number")
+    private String cardNumber;
+
+    @Column(name = "pay_date")
+    private LocalDateTime payDate;
+
+    @OneToOne(mappedBy = "payment")
+    private Order order;
+
+    public void addOrder(Order order) {
+        this.order = order;
+    }
+}
