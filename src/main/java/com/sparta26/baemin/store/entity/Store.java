d226fe645a988a36ba99c3a6c1dd5c4e7e4f345e
TrendFollow <160304124+TrendFollow@users.noreply.github.com>
2024-08-25 12:06:17 +0900

    feat Store 엔티티

---
+++ src/main/java/com/sparta26/baemin/store/entity/Store.java
@@ -0,0 +1,85 @@
+package com.sparta26.baemin.store.entity;
+
+import com.sparta26.baemin.common.AuditEntity;
+import com.sparta26.baemin.member.entity.Member;
+import com.sparta26.baemin.order.entity.Order;
+import com.sparta26.baemin.product.entity.Product;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "p_STORES")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Store extends AuditEntity {
+
+    @Id
+    @GeneratedValue @UuidGenerator
+    @Column(name = "store_id")
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+    private String description;
+
+    @Column(name = "opening_time")
+    private String openingTime;
+    @Column(name = "closing_time")
+    private String closingTime;
+    @Column(name = "open_days")
+    private String openDays;
+
+    @Column(nullable = false)
+    private String address;
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "is_active")
+    private boolean isActive = false;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "store")
+    private List<Product> products = new ArrayList<>();
+
+    @OneToMany(mappedBy = "store")
+    private List<Order> orders = new ArrayList<>();
+
+    public Store(String name, String description, String openingTime, String closingTime, String openDays, String address, String phoneNumber, Member member) {
+        this.name = name;
+        this.description = description;
+        this.openingTime = openingTime;
+        this.closingTime = closingTime;
+        this.openDays = openDays;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        if (member != null) {
+            addMember(member);
+        }
+    }
+
+    //== 생성 메서드 ==//
+    public static Store createStore(String name, String description, String openingTime, String closingTime, String openDays, String address, String phoneNumber, Member member) {
+        return new Store(name, description, openingTime, closingTime, openDays, address, phoneNumber, member);
+    }
+
+    
+    public void addMember(Member member) {
+        this.member = member;
+        member.getStores().add(this);
+    }
+
+    // 활성화 상태 변경
+    public void changeActive() {
+        this.isActive = true;
+    }
+}
