2b0c385514344ccec8cbd6af15de5eea19f4d13b
TrendFollow <160304124+TrendFollow@users.noreply.github.com>
2024-08-25 12:03:18 +0900

    feat Ai 엔티티

---
+++ src/main/java/com/sparta26/baemin/ai/entity/Ai.java
@@ -0,0 +1,26 @@
+package com.sparta26.baemin.ai.entity;
+
+import com.sparta26.baemin.common.AuditEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "p_AI")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Ai extends AuditEntity {
+
+    @Id
+    @GeneratedValue @UuidGenerator
+    @Column(name = "ai_id")
+    private UUID id;
+
+    private String question;
+    private String answer;
+
+}
