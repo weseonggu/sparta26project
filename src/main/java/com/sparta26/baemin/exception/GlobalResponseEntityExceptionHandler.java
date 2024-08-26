0b004b9f4d0894c1613fb086a6e5e2900b9d165a
TrendFollow <160304124+TrendFollow@users.noreply.github.com>
2024-08-26 11:43:27 +0900

    chore RestControllerAdvice 수정

--- src/main/java/com/sparta26/baemin/exception/GlobalResponseEntityExceptionHandler.java
+++ src/main/java/com/sparta26/baemin/exception/GlobalResponseEntityExceptionHandler.java
@@ -2,8 +2,9 @@ package com.sparta26.baemin.exception;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 }
