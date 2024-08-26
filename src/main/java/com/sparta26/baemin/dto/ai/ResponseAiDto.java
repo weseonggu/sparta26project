b7eb6ea58bc3ce8ee2e8004afb940d8777bf35a3
TrendFollow <160304124+TrendFollow@users.noreply.github.com>
2024-08-26 22:34:48 +0900

    feat ResponseAiDto 추가

---
+++ src/main/java/com/sparta26/baemin/dto/ai/ResponseAiDto.java
@@ -0,0 +1,14 @@
+package com.sparta26.baemin.dto.ai;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseAiDto {
+
+    private String answer;
+}
