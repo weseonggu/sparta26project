73ee46eb397b6ba806bea1c08fb55a18f6f8e04d
TrendFollow <160304124+TrendFollow@users.noreply.github.com>
2024-08-27 14:14:59 +0900

    feat store 로그 처리 추가

--- src/main/java/com/sparta26/baemin/aspect/LoggingAspectOfJoinStore.java
+++ src/main/java/com/sparta26/baemin/aspect/LoggingAspectOfJoinStore.java
@@ -1,7 +1,8 @@
 package com.sparta26.baemin.aspect;
 
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 /**
@@ -11,4 +12,42 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j(topic = "StoreTopic")
 public class LoggingAspectOfJoinStore {
+
+    // 두 패키지의 모든 클래스와 메서드에 적용될 포인트컷 정의
+    @Pointcut("execution(* com.sparta26.baemin.store.service..*(..)) || execution(* com.sparta26.baemin.store.controller..*(..))")
+    public void applicationPackagePointcut() {
+    }
+
+    // 메서드 실행 전 로그 기록
+    @Before("applicationPackagePointcut()")
+    public void logBefore(ProceedingJoinPoint joinPoint) {
+        String methodName = joinPoint.getSignature().getName();
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        log.info("메서드 실행 전 로그 - 클래스명: {}, 메서드명: {}", className, methodName);
+    }
+
+    // 메서드 실행 후 로그 기록
+    @After("applicationPackagePointcut()")
+    public void logAfter(ProceedingJoinPoint joinPoint) {
+        String methodName = joinPoint.getSignature().getName();
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        log.info("메서드 실행 후 로그 - 클래스명: {}, 메서드명: {}", className, methodName);
+    }
+
+    // 메서드 실행 전후로 로그 기록 (메서드 실행 시간 측정)
+    @Around("applicationPackagePointcut()")
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        String methodName = joinPoint.getSignature().getName();
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+
+        long startTime = System.currentTimeMillis();
+        log.info("메서드 실행 시작 - 클래스명: {}, 메서드명: {}", className, methodName);
+
+        Object result = joinPoint.proceed(); // 실제 메서드 실행
+
+        long elapsedTime = System.currentTimeMillis() - startTime;
+        log.info("메서드 실행 종료 - 클래스명: {}, 메서드명: {}, 실행 시간: {}ms", className, methodName, elapsedTime);
+
+        return result;
+    }
 }
