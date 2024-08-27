816ff9e6bc5dc533b8a00776a0a4e56b9cf51cf7
TrendFollow <160304124+TrendFollow@users.noreply.github.com>
2024-08-27 14:18:14 +0900

    chore 상수 전체 변경

--- src/main/java/com/sparta26/baemin/member/entity/UserRole.java
+++ src/main/java/com/sparta26/baemin/member/entity/UserRole.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 
 @Getter
 public enum UserRole {
-//  고객     가게주인   관리자
-    CUSTOMER_ROLE, OWNER_ROLE, MASTER_ROLE, USER_ROLE
-
+//  고객             가게주인      관리자         매니저
+    ROLE_CUSTOMER, ROLE_OWNER, ROLE_MASTER, ROLE_MANAGER;
 }
