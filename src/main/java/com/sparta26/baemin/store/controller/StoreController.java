0c33f179dc9a272618cbe2f215731c7f7d732196
TrendFollow <160304124+TrendFollow@users.noreply.github.com>
2024-08-27 14:17:46 +0900

    feat 가게 생성 권한 수정

--- src/main/java/com/sparta26/baemin/store/controller/StoreController.java
+++ src/main/java/com/sparta26/baemin/store/controller/StoreController.java
@@ -2,11 +2,15 @@ package com.sparta26.baemin.store.controller;
 
 import com.sparta26.baemin.dto.store.RequestStoreDto;
 import com.sparta26.baemin.dto.store.ResponseStoreDto;
+import com.sparta26.baemin.exception.exceptionsdefined.ForbiddenAccessException;
+import com.sparta26.baemin.jwt.CustomUserDetails;
+import com.sparta26.baemin.jwt.ForContext;
 import com.sparta26.baemin.store.service.StoreService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,11 +22,21 @@ public class StoreController {
     private final StoreService storeService;
 
     // 가게 생성
-    @PostMapping("/store/{memberId}")
+    @PostMapping("/store")
     public ResponseEntity<?> createStore(@Valid @RequestBody RequestStoreDto requestStoreDto,
-                                         @PathVariable Long memberId) {
+                                         @AuthenticationPrincipal CustomUserDetails customUserDetails ) {
+        log.info("가게 생성 시도 중 | requestStoreDto = {} | Member = {}", requestStoreDto, customUserDetails.getForContext());
+        ForContext context = customUserDetails.getForContext();
+        Long memberId = context.getId();
+        String role = context.getRole();
 
-        ResponseStoreDto responseStoreDto = storeService.createStore(requestStoreDto, memberId);
+        if (!role.equals("ROLE_OWNER") && !role.equals("ROLE_MASTER")) {
+            log.error("사용자 권한 불일치 = {}", context.getRole());
+            throw new ForbiddenAccessException("Unauthorized user");
+        }
+
+        ResponseStoreDto responseStoreDto = storeService.createStore(requestStoreDto,memberId);
+        log.info("가게 생성 완료 | responseStoreDto = {} | Member = {}", responseStoreDto, memberId);
         return ResponseEntity.ok(responseStoreDto);
     }
 
@@ -33,9 +47,5 @@ public class StoreController {
         ResponseStoreDto responseStoreDto = storeService.findOneStore(storeId);
         return ResponseEntity.ok(responseStoreDto);
     }
-
-    // 가게 전체 조회
-    
-
 
 }
