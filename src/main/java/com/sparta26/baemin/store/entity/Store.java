8dbbf7c6d0af26a2322ea8a0b1363d34bf65121a
TrendFollow <160304124+TrendFollow@users.noreply.github.com>
2024-08-28 01:26:11 +0900

    chore OperatingHours 엔티티와 연관관계 매핑

--- src/main/java/com/sparta26/baemin/store/entity/Store.java
+++ src/main/java/com/sparta26/baemin/store/entity/Store.java
@@ -3,13 +3,16 @@ package com.sparta26.baemin.store.entity;
 import com.sparta26.baemin.category.entity.Category;
 import com.sparta26.baemin.common.entity.AuditEntity;
 import com.sparta26.baemin.deliveryzone.entity.DeliveryZone;
+import com.sparta26.baemin.dto.store.RequestStoreDto;
 import com.sparta26.baemin.member.entity.Member;
+import com.sparta26.baemin.operatinghours.entity.OperatingHours;
 import com.sparta26.baemin.order.entity.Order;
 import com.sparta26.baemin.product.entity.Product;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.hibernate.annotations.UuidGenerator;
 
 import java.util.ArrayList;
@@ -20,6 +23,7 @@ import java.util.UUID;
 @Table(name = "p_STORES")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id","name","description","address","phoneNumber","isActive"})
 public class Store extends AuditEntity {
 
     @Id
@@ -31,15 +35,6 @@ public class Store extends AuditEntity {
     private String name;
 
     private String description;
-
-    @Column(name = "opening_time")
-    private String openingTime;
-
-    @Column(name = "closing_time")
-    private String closingTime;
-
-    @Column(name = "open_days")
-    private String openDays;
 
     @Column(nullable = false, unique = true)
     private String address;
@@ -66,12 +61,12 @@ public class Store extends AuditEntity {
     @OneToMany(mappedBy = "store")
     private List<Category> categories = new ArrayList<>();
 
-    public Store(String name, String description, String openingTime, String closingTime, String openDays, String address, String phoneNumber, Member member) {
+    @OneToMany(mappedBy = "store")
+    private List<OperatingHours> operatingHours = new ArrayList<>();
+
+    public Store(String name, String description, String address, String phoneNumber, Member member) {
         this.name = name;
         this.description = description;
-        this.openingTime = openingTime;
-        this.closingTime = closingTime;
-        this.openDays = openDays;
         this.address = address;
         this.phoneNumber = phoneNumber;
         if (member != null) {
@@ -83,13 +78,33 @@ public class Store extends AuditEntity {
     /**
      * 생성 메서드
      */
-    public static Store createStore(String name, String description, String openingTime, String closingTime, String openDays, String address, String phoneNumber, Member member) {
-        return new Store(name, description, openingTime, closingTime, openDays, address, phoneNumber, member);
+    public static Store createStore(String name, String description, String address, String phoneNumber, Member member) {
+        return new Store(name, description, address, phoneNumber, member);
     }
-
 
     // 활성화 상태 변경
     public void changeActive() {
-        this.isActive = true;
+        if (this.isActive) {
+            this.isActive = false;
+        }else {
+            this.isActive = true;
+        }
+    }
+
+    public Store update(RequestStoreDto requestStoreDto, String email) {
+        if (requestStoreDto.getName() != null) {
+            this.name = requestStoreDto.getName();
+        }
+        if (requestStoreDto.getDescription() != null) {
+            this.description = requestStoreDto.getDescription();
+        }
+        if (requestStoreDto.getAddress() != null) {
+            this.address = requestStoreDto.getAddress();
+        }
+        if (requestStoreDto.getPhone_number() != null) {
+            this.phoneNumber = requestStoreDto.getPhone_number();
+        }
+        super.addUpdatedBy(email);
+        return this;
     }
 }
