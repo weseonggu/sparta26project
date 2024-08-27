5c7cd2e1fdc2e20aff1772a0d93fb58134d33d38
TrendFollow <160304124+TrendFollow@users.noreply.github.com>
2024-08-28 01:24:30 +0900

    chore 운영시간 필드 삭제

--- src/main/java/com/sparta26/baemin/dto/store/RequestStoreDto.java
+++ src/main/java/com/sparta26/baemin/dto/store/RequestStoreDto.java
@@ -1,25 +1,17 @@
 package com.sparta26.baemin.dto.store;
 
-import com.sparta26.baemin.store.entity.Store;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-@Getter @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class RequestStoreDto {
 
     @NotBlank(message = "가게 이름은 필수 입니다.")
     private String name;
-    
     private String description;
-    private String opening_time;
-    private String closing_time;
-    private String open_days;
-    
+
     @NotBlank(message = "가게 주소는 필수 입니다.")
     private String address;
     @NotBlank(message = "가게 번호는 필수 입니다.")
