08808039e624c1e55520e421a7880d3a82ead258
TrendFollow <160304124+TrendFollow@users.noreply.github.com>
2024-08-28 14:38:32 +0900

    feat ProductRepository 추가

---
+++ src/main/java/com/sparta26/baemin/product/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.sparta26.baemin.product.repository;
+
+import com.sparta26.baemin.product.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+}
