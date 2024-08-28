033f16c8b26e726adf1993009784b00889efdd39
krystal-13 <honey9163@gmail.com>
2024-08-28 22:36:24 +0900

    feat 문자열에서 OrderStatus 로 변환하는 로직 구현

--- src/main/java/com/sparta26/baemin/order/entity/OrderStatus.java
+++ src/main/java/com/sparta26/baemin/order/entity/OrderStatus.java
@@ -1,8 +1,20 @@
 package com.sparta26.baemin.order.entity;
 
+import lombok.Getter;
+
+@Getter
 public enum OrderStatus {
     PENDING,
     CONFIRM,
     COMPLETE,
-    CANCEL
+    CANCEL;
+
+    public static OrderStatus fromString(String value) {
+        for (OrderStatus status : OrderStatus.values()) {
+            if (status.name().equalsIgnoreCase(value)) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("Unknown order status: " + value);
+    }
 }
