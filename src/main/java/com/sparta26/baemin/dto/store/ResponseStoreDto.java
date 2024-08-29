ddb0d1d60aab080a788fb6fb9f5f56468774d49c
TrendFollow <160304124+TrendFollow@users.noreply.github.com>
2024-08-29 15:36:11 +0900

    chore List<ResponseOperatingDto> operatingHours 추가

--- src/main/java/com/sparta26/baemin/dto/store/ResponseStoreDto.java
+++ src/main/java/com/sparta26/baemin/dto/store/ResponseStoreDto.java
@@ -1,7 +1,14 @@
 package com.sparta26.baemin.dto.store;
 
+import com.sparta26.baemin.dto.operatinghours.ResponseOperatingDto;
 import com.sparta26.baemin.store.entity.Store;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -15,13 +22,20 @@ public class ResponseStoreDto {
     private String address;
     private String phone_number;
     private boolean is_active;
+    private List<ResponseOperatingDto> operatingHours = new ArrayList<>();
+
 
     public static ResponseStoreDto toDto(Store store) {
-        return new ResponseStoreDto(store.getId().toString(),
+        List<ResponseOperatingDto> operatingHours = store.getOperatingHours().stream()
+                .map(ResponseOperatingDto::toDto)
+                .collect(Collectors.toList());
+
+        return  new ResponseStoreDto(store.getId().toString(),
                 store.getName(),
                 store.getDescription(),
                 store.getAddress(),
                 store.getPhoneNumber(),
-                store.isActive());
+                store.isActive(),
+                operatingHours);
     }
 }
