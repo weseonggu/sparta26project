52bbcad72d91686492387ccd69438464eb07c060
TrendFollow <160304124+TrendFollow@users.noreply.github.com>
2024-08-29 15:29:51 +0900

    chore isPublic = true 조회 추가

--- src/main/java/com/sparta26/baemin/operatinghours/repository/OperatingHoursRepository.java
+++ src/main/java/com/sparta26/baemin/operatinghours/repository/OperatingHoursRepository.java
@@ -3,6 +3,7 @@ package com.sparta26.baemin.operatinghours.repository;
 import com.sparta26.baemin.operatinghours.entity.OperatingHours;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,9 +11,15 @@ import java.util.UUID;
 
 public interface OperatingHoursRepository extends JpaRepository<OperatingHours, UUID> {
 
-    OperatingHours findByStoreIdAndOpenDays(UUID store_id, String openDays);
-    Optional<OperatingHours> findByIdAndCreatedBy(UUID id, String createdBy);
+    @Query("select o from OperatingHours o where o.id = :id and o.openDays = :openDays and o.isPublic = true")
+    OperatingHours findByStoreIdAndOpenDays(@Param("id") UUID id,@Param("openDays") String openDays);
+
+    @Query("select o from OperatingHours o where o.id = :id and o.createdBy = :createdBy and o.isPublic = true")
+    Optional<OperatingHours> findByIdAndCreatedBy(@Param("id") UUID id,@Param("createdBy") String createdBy);
 
     @Query("select o from OperatingHours o where o.isPublic = true")
     List<OperatingHours> findByStoreId(UUID store_id);
+
+    @Query("select o from OperatingHours o where o.isPublic = true and o.id = :id")
+    Optional<OperatingHours> findIsPublicById(@Param("id") UUID id);
 }
