19461cd93930b789f71d2712a08aa833ec4367d5
TrendFollow <160304124+TrendFollow@users.noreply.github.com>
2024-08-29 20:16:51 +0900

    chore LoggingAspectOfJoinAi 에러 로그 추가

--- src/main/java/com/sparta26/baemin/aspect/LoggingAspectOfJoinAi.java
+++ src/main/java/com/sparta26/baemin/aspect/LoggingAspectOfJoinAi.java
@@ -14,25 +14,10 @@ import org.springframework.stereotype.Component;
 public class LoggingAspectOfJoinAi {
 
     // 두 패키지의 모든 클래스와 메서드에 적용될 포인트컷 정의
-    @Pointcut("execution(* com.sparta26.baemin.ai.service..*(..)) || execution(* com.sparta26.baemin.ai.controller..*(..))")
+    @Pointcut("execution(* com.sparta26.baemin.ai..*(..))")
     public void applicationPackagePointcut() {
     }
 
-    // 메서드 실행 전 로그 기록
-    @Before("applicationPackagePointcut()")
-    public void logBefore(ProceedingJoinPoint joinPoint) {
-        String methodName = joinPoint.getSignature().getName();
-        String className = joinPoint.getSignature().getDeclaringTypeName();
-        log.info("메서드 실행 전 로그 - 클래스명: {}, 메서드명: {}", className, methodName);
-    }
-
-    // 메서드 실행 후 로그 기록
-    @After("applicationPackagePointcut()")
-    public void logAfter(ProceedingJoinPoint joinPoint) {
-        String methodName = joinPoint.getSignature().getName();
-        String className = joinPoint.getSignature().getDeclaringTypeName();
-        log.info("메서드 실행 후 로그 - 클래스명: {}, 메서드명: {}", className, methodName);
-    }
 
     // 메서드 실행 전후로 로그 기록 (메서드 실행 시간 측정)
     @Around("applicationPackagePointcut()")
@@ -43,7 +28,13 @@ public class LoggingAspectOfJoinAi {
         long startTime = System.currentTimeMillis();
         log.info("메서드 실행 시작 - 클래스명: {}, 메서드명: {}", className, methodName);
 
-        Object result = joinPoint.proceed(); // 실제 메서드 실행
+        Object result = null;
+        try {
+            result = joinPoint.proceed(); // 실제 메서드 실행
+        } catch (Throwable ex) {
+            log.error("예외 발생 - 클래스명: {}, 메서드명: {}, 예외 메시지: {}", className, methodName, ex.getMessage());
+            throw ex; // 예외를 다시 던져줍니다.
+        }
 
         long elapsedTime = System.currentTimeMillis() - startTime;
         log.info("메서드 실행 종료 - 클래스명: {}, 메서드명: {}, 실행 시간: {}ms", className, methodName, elapsedTime);
