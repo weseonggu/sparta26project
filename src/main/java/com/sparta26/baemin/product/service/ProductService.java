13650cd38df2ff306bd8f3e67f1b8afc98fd1986
TrendFollow <160304124+TrendFollow@users.noreply.github.com>
2024-08-29 15:31:45 +0900

    chore 상품 수정 추가

--- src/main/java/com/sparta26/baemin/product/service/ProductService.java
+++ src/main/java/com/sparta26/baemin/product/service/ProductService.java
@@ -1,11 +1,11 @@
 package com.sparta26.baemin.product.service;
 
-import com.sparta26.baemin.dto.member.ResponseMemberToProductDto;
 import com.sparta26.baemin.dto.product.RequestProductDto;
+import com.sparta26.baemin.dto.product.RequestProductWithoutStockDto;
 import com.sparta26.baemin.dto.product.ResponseProductDto;
 import com.sparta26.baemin.dto.store.ResponseStoreDto;
-import com.sparta26.baemin.member.entity.Member;
-import com.sparta26.baemin.member.entity.UserRole;
+import com.sparta26.baemin.exception.exceptionsdefined.ProductNotFoundException;
+import com.sparta26.baemin.exception.exceptionsdefined.UuidFormatException;
 import com.sparta26.baemin.product.clinet.ProductToMemberClient;
 import com.sparta26.baemin.product.clinet.ProductToStoreClient;
 import com.sparta26.baemin.product.entity.Product;
@@ -40,16 +40,8 @@ public class ProductService {
     public ResponseProductDto createProduct(RequestProductDto request, String storeId, Long memberId, String email) {
         // product 세팅을 위한 조회
         ResponseStoreDto findStore = productToStoreClient.findByIdAndMemberId(storeId, memberId);
-        ResponseMemberToProductDto findMember = productToMemberClient.findById(memberId);
 
-        Member member = new Member(findMember.getMember_id(),findMember.getEmail(), findMember.getUsername(), findMember.getNickname(), UserRole.valueOf(findMember.getRole()));
-        Store store = new Store(UUID.fromString(findStore.getId()),
-                findStore.getName(),
-                findStore.getDescription(),
-                findStore.getAddress(),
-                findStore.getPhone_number(),
-                findStore.is_active(),
-                member);
+        Store store = new Store(UUID.fromString(findStore.getId()));
 
         Product product = Product.createProduct(request.getName(),
                 request.getDescription(),
@@ -62,5 +54,43 @@ public class ProductService {
 
         Product savedProduct = productRepository.save(product);
         return ResponseProductDto.toDto(savedProduct);
+    }
+
+
+    @Transactional
+    public ResponseProductDto updateProduct(RequestProductWithoutStockDto request, Long memberId, String email, String role, String productId) {
+
+        if (!isValidUUID(productId)) {
+            log.error("Invalid UUID = {}",productId);
+            throw new UuidFormatException("Invalid UUID format");
+        }
+
+        Product findProduct = productRepository.findById(UUID.fromString(productId)).orElseThrow(() ->
+                new ProductNotFoundException("not found product"));
+
+        Product product = productRepository.findById(UUID.fromString(productId)).orElseThrow(() -> new ProductNotFoundException("not found product"));
+
+        if (role.equals("ROLE_OWNER")) {
+            // 가게 주인 본인이 수정시 본인 확인 메서드
+            ResponseStoreDto owner = productToStoreClient.findByIdAndMemberId(findProduct.getStore().getId().toString(), memberId);
+            product.update(request, email);
+        }else {
+            product.update(request, email);
+        }
+        return ResponseProductDto.toDto(product);
+    }
+
+    /**
+     * UUID 형식 검증 메서드
+     * @param storeId
+     * @return
+     */
+    public boolean isValidUUID(String storeId) {
+        try {
+            UUID.fromString(storeId);
+            return true;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
     }
 }
