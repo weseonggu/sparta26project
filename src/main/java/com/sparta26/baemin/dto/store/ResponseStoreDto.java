c1ba202bdffc8898dbf738120df447937a29ffca
TrendFollow <160304124+TrendFollow@users.noreply.github.com>
2024-08-29 20:20:07 +0900

    chore ResponseStoreDto 운영시간 리스트 삭제

--- src/main/java/com/sparta26/baemin/dto/store/ResponseStoreDto.java
+++ src/main/java/com/sparta26/baemin/dto/store/ResponseStoreDto.java
@@ -1,14 +1,9 @@
 package com.sparta26.baemin.dto.store;
 
-import com.sparta26.baemin.dto.operatinghours.ResponseOperatingDto;
 import com.sparta26.baemin.store.entity.Store;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -22,20 +17,14 @@ public class ResponseStoreDto {
     private String address;
     private String phone_number;
     private boolean is_active;
-    private List<ResponseOperatingDto> operatingHours = new ArrayList<>();
 
 
     public static ResponseStoreDto toDto(Store store) {
-        List<ResponseOperatingDto> operatingHours = store.getOperatingHours().stream()
-                .map(ResponseOperatingDto::toDto)
-                .collect(Collectors.toList());
-
         return  new ResponseStoreDto(store.getId().toString(),
                 store.getName(),
                 store.getDescription(),
                 store.getAddress(),
                 store.getPhoneNumber(),
-                store.isActive(),
-                operatingHours);
+                store.isActive());
     }
 }
