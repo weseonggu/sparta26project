a23d54f7e390e9267788498f76e0d02a3024a1a4
TrendFollow <160304124+TrendFollow@users.noreply.github.com>
2024-08-29 15:30:39 +0900

    chore 상품 수정 추가

--- src/main/java/com/sparta26/baemin/product/controller/ProductController.java
+++ src/main/java/com/sparta26/baemin/product/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.sparta26.baemin.product.controller;
 
 import com.sparta26.baemin.dto.product.RequestProductDto;
+import com.sparta26.baemin.dto.product.RequestProductWithoutStockDto;
 import com.sparta26.baemin.dto.product.ResponseProductDto;
 import com.sparta26.baemin.jwt.CustomUserDetails;
 import com.sparta26.baemin.jwt.ForContext;
@@ -8,6 +9,7 @@ import com.sparta26.baemin.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,6 +23,7 @@ public class ProductController {
 
     /**
      * 상품 등록, 가게 주인만 허용
+     *
      * @param request
      * @param customUserDetails
      * @param storeId
@@ -36,6 +39,22 @@ public class ProductController {
         String email = context.getEmail();
 
         ResponseProductDto response = productService.createProduct(request, storeId, memberId, email);
+        return ResponseEntity.ok(response);
+    }
+
+
+    @Secured({"ROLE_OWNER", "ROLE_MASTER"})
+    @PatchMapping("/products/{productId}")
+    public ResponseEntity<?> updateProduct(@RequestBody RequestProductWithoutStockDto request,
+                                           @AuthenticationPrincipal CustomUserDetails customUserDetails,
+                                           @PathVariable("productId") String productId) {
+
+        ForContext context = customUserDetails.getForContext();
+        Long memberId = context.getId();
+        String email = context.getEmail();
+        String role = context.getRole();
+
+        ResponseProductDto response = productService.updateProduct(request, memberId, email, role, productId);
         return ResponseEntity.ok(response);
     }
 }
