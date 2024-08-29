83a2dcd0eb3acce5b2bb60fe8666b5b2d30f4780
TrendFollow <160304124+TrendFollow@users.noreply.github.com>
2024-08-29 20:19:57 +0900

    chore RequestStoreDto 운영시간 리스트 삭제

--- src/main/java/com/sparta26/baemin/dto/store/RequestStoreDto.java
+++ src/main/java/com/sparta26/baemin/dto/store/RequestStoreDto.java
@@ -1,14 +1,9 @@
 package com.sparta26.baemin.dto.store;
 
-import com.sparta26.baemin.dto.operatinghours.RequestOperatingHoursDto;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -23,10 +18,5 @@ public class RequestStoreDto {
     private String address;
     @NotBlank(message = "가게 번호는 필수 입니다.")
     private String phone_number;
-
-    @NotEmpty(message = "운영 시간은 최소 하나 이상 필수 입니다.")
-    @Valid
-    private List<RequestOperatingHoursDto> operating_hours;
-
 
 }
