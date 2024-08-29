d5c35548c7738be212e962035daab5d766145960
TrendFollow <160304124+TrendFollow@users.noreply.github.com>
2024-08-30 02:19:33 +0900

    feat ResponseProductDto UUID 변경

--- src/main/java/com/sparta26/baemin/dto/product/ResponseProductDto.java
+++ src/main/java/com/sparta26/baemin/dto/product/ResponseProductDto.java
@@ -1,16 +1,17 @@
 package com.sparta26.baemin.dto.product;
 
+import com.querydsl.core.annotations.QueryProjection;
 import com.sparta26.baemin.product.entity.Product;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class ResponseProductDto {
 
-    private String product_id;
+    private UUID product_id;
     private String name;
     private String description;
 
@@ -21,8 +22,20 @@ public class ResponseProductDto {
     private String imageUrl;
     private boolean is_available;
 
+    @QueryProjection
+    public ResponseProductDto(UUID product_id, String name, String description, Integer price, Integer stock_quantity, String category, String imageUrl, boolean is_available) {
+        this.product_id = product_id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stock_quantity = stock_quantity;
+        this.category = category;
+        this.imageUrl = imageUrl;
+        this.is_available = is_available;
+    }
+
     public static ResponseProductDto toDto(Product savedProduct) {
-        return new ResponseProductDto(savedProduct.getId().toString(),
+        return new ResponseProductDto(savedProduct.getId(),
                 savedProduct.getName(),
                 savedProduct.getDescription(),
                 savedProduct.getPrice(),
