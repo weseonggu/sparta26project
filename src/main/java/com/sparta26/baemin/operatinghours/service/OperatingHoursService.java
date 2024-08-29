ce05b01eda96cff34902fc9f0eb30e802e411268
TrendFollow <160304124+TrendFollow@users.noreply.github.com>
2024-08-29 20:18:30 +0900

    chore OperatingHoursService 등록 재수정

--- src/main/java/com/sparta26/baemin/operatinghours/service/OperatingHoursService.java
+++ src/main/java/com/sparta26/baemin/operatinghours/service/OperatingHoursService.java
@@ -2,9 +2,12 @@ package com.sparta26.baemin.operatinghours.service;
 
 import com.sparta26.baemin.dto.operatinghours.RequestOperatingHoursDto;
 import com.sparta26.baemin.dto.operatinghours.ResponseOperatingDto;
+import com.sparta26.baemin.exception.exceptionsdefined.StoreNotFoundException;
 import com.sparta26.baemin.exception.exceptionsdefined.UuidFormatException;
 import com.sparta26.baemin.operatinghours.entity.OperatingHours;
 import com.sparta26.baemin.operatinghours.repository.OperatingHoursRepository;
+import com.sparta26.baemin.store.entity.Store;
+import com.sparta26.baemin.store.repository.StoreRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -21,6 +24,45 @@ import java.util.UUID;
 public class OperatingHoursService {
 
     private final OperatingHoursRepository operatingHoursRepository;
+    private final StoreRepository storeRepository;
+
+    /**
+     * 가게 운영시간 등록, 가게주인만 생성가능
+     * @param requestOperatingHoursDto
+     * @param storeId
+     * @param memberId
+     * @param email
+     * @return
+     */
+    @Transactional
+    public ResponseOperatingDto createOperatingHours(RequestOperatingHoursDto requestOperatingHoursDto, String storeId, Long memberId, String email) {
+
+        if (!isValidUUID(storeId)) {
+            log.error("UUID = {}", storeId);
+            throw new UuidFormatException("UUID 형식이 틀렸습니다.");
+        }
+
+        Store store = storeRepository.findByIdAndMemberId(UUID.fromString(storeId), memberId).orElseThrow(() ->
+                new StoreNotFoundException("본인의 상점만 등록 가능합니다."));
+
+        OperatingHours findOperating = operatingHoursRepository.findByStoreIdAndOpenDays(UUID.fromString(storeId),
+                requestOperatingHoursDto.getOpen_days());
+
+        if (findOperating != null) {
+            log.error("등록 시도한 요일 : {}", requestOperatingHoursDto.getOpen_days());
+            throw new IllegalArgumentException(requestOperatingHoursDto.getOpen_days() + "은 이미 등록되어있습니다.");
+        }
+
+        OperatingHours operatingHours = new OperatingHours(requestOperatingHoursDto.getOpening_time(),
+                requestOperatingHoursDto.getClosing_time(),
+                requestOperatingHoursDto.getOpen_days(),
+                requestOperatingHoursDto.getLast_order(),
+                store,
+                email);
+
+        OperatingHours savedOperating = operatingHoursRepository.save(operatingHours);
+        return ResponseOperatingDto.toDto(savedOperating);
+    }
 
     /**
      * 가게 운영시간 수정, 가게주인 관리자 가능
@@ -39,6 +81,7 @@ public class OperatingHoursService {
 
         OperatingHours updateOperating;
         if (role.equals("ROLE_OWNER")) {
+
             OperatingHours findOperating = operatingHoursRepository.findByIdAndCreatedBy(UUID.fromString(operatingId), email)
                     .orElseThrow(() -> new IllegalArgumentException("not found OperatingHours or Invalid user"));
 
