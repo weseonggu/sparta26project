1d59a01aa331ee48e19d6838251dda7a56915078
TrendFollow <160304124+TrendFollow@users.noreply.github.com>
2024-08-29 15:28:39 +0900

    chore 가게 > 운영시간 단방향 코드 수정

--- src/main/java/com/sparta26/baemin/operatinghours/entity/OperatingHours.java
+++ src/main/java/com/sparta26/baemin/operatinghours/entity/OperatingHours.java
@@ -2,7 +2,7 @@ package com.sparta26.baemin.operatinghours.entity;
 
 import com.sparta26.baemin.common.entity.AuditEntity;
 import com.sparta26.baemin.dto.operatinghours.RequestOperatingHoursDto;
-import com.sparta26.baemin.store.entity.Store;
+import com.sparta26.baemin.dto.operatinghours.ResponseOperatingDto;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -35,25 +35,53 @@ public class OperatingHours extends AuditEntity {
     @Column(name = "last_order")
     private String lastOrder;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "store_id")
-    public Store store;
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "store_id")
+//    public Store store;
 
-    public OperatingHours(String openingTime, String closingTime, String openDays, String lastOrder, Store store, String email) {
+    public OperatingHours(String openingTime, String closingTime, String openDays, String lastOrder, String email) {
         this.openingTime = openingTime;
         this.closingTime = closingTime;
         this.openDays = openDays;
         this.lastOrder = lastOrder;
-        if(store != null) {
-            addStore(store);
-        }
         super.addCreatedBy(email);
     }
 
-    public void addStore(Store store) {
-        this.store = store;
-        store.getOperatingHours().add(this);
+    /**
+     * 운영시간 생성 시 사용!!
+     */
+
+//    public OperatingHours(String openingTime, String closingTime, String openDays, String lastOrder, Store store, String email) {
+//        this.openingTime = openingTime;
+//        this.closingTime = closingTime;
+//        this.openDays = openDays;
+//        this.lastOrder = lastOrder;
+//        if(store != null) {
+//            addStore(store);
+//        }
+//        super.addCreatedBy(email);
+//    }
+
+    public OperatingHours(UUID id, String openingTime, String closingTime, String openDays, String lastOrder) {
+        this.id = id;
+        this.openingTime = openingTime;
+        this.closingTime = closingTime;
+        this.openDays = openDays;
+        this.lastOrder = lastOrder;
     }
+
+    public static OperatingHours toEntity(ResponseOperatingDto response) {
+        return new OperatingHours(UUID.fromString(response.getOperating_hours_id()),
+                response.getOpening_time(),
+                response.getClosing_time(),
+                response.getOpen_days(),
+                response.getLast_order());
+    }
+
+//    public void addStore(Store store) {
+//        this.store = store;
+//        store.getOperatingHours().add(this);
+//    }
 
     /**
      * 업데이트 메서드
