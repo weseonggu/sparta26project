6e1e8dd1737d63b7a4561fd0a18ad21984498915
TrendFollow <160304124+TrendFollow@users.noreply.github.com>
2024-08-30 02:19:01 +0900

    chore 삭제

--- src/main/java/com/sparta26/baemin/product/clinet/ProductToMemberClient.java
+++
@@ -1,7 +0,0 @@
-package com.sparta26.baemin.product.clinet;
-
-import com.sparta26.baemin.dto.member.ResponseMemberToProductDto;
-
-public interface ProductToMemberClient {
-    ResponseMemberToProductDto findById(Long memberId);
-}
