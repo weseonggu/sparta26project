a60dee9fb00012d4d6637f12cfd958a083e8d2e5
TrendFollow <160304124+TrendFollow@users.noreply.github.com>
2024-09-01 12:33:02 +0900

    chore 변경 없음

--- src/main/java/com/sparta26/baemin/store/repository/StoreRepository.java
+++ src/main/java/com/sparta26/baemin/store/repository/StoreRepository.java
@@ -1,8 +1,6 @@
 package com.sparta26.baemin.store.repository;
 
 import com.sparta26.baemin.store.entity.Store;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -11,7 +9,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface StoreRepository extends JpaRepository<Store, UUID> {
+public interface StoreRepository extends JpaRepository<Store, UUID>, StoreRepositoryCustom {
 
     @Query("select s from Store s where (s.address = :address or s.phoneNumber = :phoneNumber) and s.isPublic = true and s.member.id <> :memberId")
     Optional<Store> findByAddressOrPhoneNumberAndMemberIdNot(@Param("address") String address,
@@ -20,12 +18,6 @@ public interface StoreRepository extends JpaRepository<Store, UUID> {
 
     @Query("select s from Store s  where s.id = :id and s.member.id = :member_id and s.isPublic = true")
     Optional<Store> findByIdAndMemberId(@Param("id") UUID id, @Param("member_id") Long member_id);
-
-    @Query("select s from Store s left join fetch s.operatingHours o where s.id = :id and s.isPublic = true and o.isPublic = true")
-    Page<Store> findByIdWithOperatingHours(@Param("id") UUID storeId, Pageable pageable);
-
-    @Query("select s from Store s left join fetch s.operatingHours o where s.isPublic = true and o.isPublic = true")
-    Page<Store> findPagingAll(Pageable pageable);
 
     @Query("select s from Store s where s.member.id = :memberId and s.isPublic = true")
     Optional<Store> findByMemberId(@Param("memberId") Long memberId);
