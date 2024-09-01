2cc92aeb0bc9e173359f960f82d2ac7ae53fa827
krystal-13 <honey9163@gmail.com>
2024-09-01 12:55:24 +0900

    feat 주문에서 productId 로 상품 정보 가져오기 위해 필요한 생성자 추가

--- src/main/java/com/sparta26/baemin/order/client/ProductServiceClientImpl.java
+++ src/main/java/com/sparta26/baemin/order/client/ProductServiceClientImpl.java
@@ -1,19 +1,36 @@
 package com.sparta26.baemin.order.client;
 
+import com.sparta26.baemin.dto.product.ResponseProductDto;
 import com.sparta26.baemin.product.entity.Product;
+import com.sparta26.baemin.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class ProductServiceClientImpl implements ProductServiceClient {
 
-    Product product;
-
-    //    private final ProductService productService;
+    private final ProductService productService;
     @Override
     public Product getProductById(String productId) {
-        //TODO ProductService 에서 productId 로 상품정보를 가져오는 메서드
-        return product;
+
+        ResponseProductDto responseProductDto =
+                productService.findOneProduct(UUID.fromString(productId));
+
+        return convertToProduct(responseProductDto);
+    }
+
+    private Product convertToProduct(ResponseProductDto response) {
+        return Product.createProductWithId(
+                response.getProduct_id(),
+                response.getName(),
+                response.getDescription(),
+                response.getPrice(),
+                response.getStock_quantity(),
+                response.getCategory(),
+                response.getImageUrl()
+        );
     }
 }
