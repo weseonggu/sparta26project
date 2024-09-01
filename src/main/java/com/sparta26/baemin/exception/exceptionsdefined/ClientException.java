6f9289ff530ac9bdf9c4c38d4b7cb1113e0697ed
krystal-13 <honey9163@gmail.com>
2024-09-01 12:56:25 +0900

    feat Client 예외 추가 및 공통 예외 통합

---
+++ src/main/java/com/sparta26/baemin/exception/exceptionsdefined/ClientException.java
@@ -0,0 +1,7 @@
+package com.sparta26.baemin.exception.exceptionsdefined;
+
+public class ClientException extends RuntimeException {
+    public ClientException(String message) {
+        super(message);
+    }
+}
