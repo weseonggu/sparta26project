6b67a5a970ced224b7a7f44a9d929c734ce8205b
TrendFollow <160304124+TrendFollow@users.noreply.github.com>
2024-09-01 12:31:09 +0900

    chore QueryProjection 추가

--- src/main/java/com/sparta26/baemin/dto/operatinghours/ResponseOperatingDto.java
+++ src/main/java/com/sparta26/baemin/dto/operatinghours/ResponseOperatingDto.java
@@ -1,26 +1,48 @@
 package com.sparta26.baemin.dto.operatinghours;
 
+import com.querydsl.core.annotations.QueryProjection;
 import com.sparta26.baemin.operatinghours.entity.OperatingHours;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class ResponseOperatingDto {
 
-    private String operating_hours_id;
+    private UUID operating_hours_id;
     private String opening_time;
     private String closing_time;
     private String open_days;
     private String last_order;
 
+    @QueryProjection
+    public ResponseOperatingDto(UUID operating_hours_id, String opening_time, String closing_time, String open_days, String last_order) {
+        this.operating_hours_id = operating_hours_id;
+        this.opening_time = opening_time;
+        this.closing_time = closing_time;
+        this.open_days = open_days;
+        this.last_order = last_order;
+    }
+
     public static ResponseOperatingDto toDto(OperatingHours savedOperating) {
-        return new ResponseOperatingDto(savedOperating.getId().toString(),
+        return new ResponseOperatingDto(savedOperating.getId(),
                 savedOperating.getOpeningTime(),
                 savedOperating.getClosingTime(),
                 savedOperating.getOpenDays(),
                 savedOperating.getLastOrder());
+    }
+
+    public static List<ResponseOperatingDto> toDtoList(List<OperatingHours> operatingHours) {
+        List<ResponseOperatingDto> dtos = new ArrayList<>();
+        for (OperatingHours operatingHour : operatingHours) {
+            if (operatingHour.isPublic()) {
+                dtos.add(toDto(operatingHour));
+            }
+        }
+        return dtos;
     }
 }
