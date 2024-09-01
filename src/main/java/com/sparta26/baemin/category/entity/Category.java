57802aad83c78e1c1bc72ed95605afa171f5d90d
TrendFollow <160304124+TrendFollow@users.noreply.github.com>
2024-09-01 12:19:37 +0900

    chore 연관관계 메서드 추가

--- src/main/java/com/sparta26/baemin/category/entity/Category.java
+++ src/main/java/com/sparta26/baemin/category/entity/Category.java
@@ -1,6 +1,7 @@
 package com.sparta26.baemin.category.entity;
 
 import com.sparta26.baemin.common.entity.AuditEntity;
+import com.sparta26.baemin.dto.category.RequestCategoryDto;
 import com.sparta26.baemin.store.entity.Store;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -28,11 +29,26 @@ public class Category extends AuditEntity {
     @JoinColumn(name = "store_id")
     private Store store;
 
-
     public Category(String name, Store store) {
-        this.id = UUID.randomUUID();
         this.name = name;
-        this.store = store;
+        if (store != null) {
+            this.store = store;
+        }
     }
 
+    public static Category createCategory(String name, Store store) {
+        return new Category(name, store);
+    }
+
+    public void addStore(Store store) {
+        this.store = store;
+        store.getCategories().add(this);
+    }
+
+    public Category update(RequestCategoryDto request) {
+        if (request.getName() != null) {
+            this.name = request.getName();
+        }
+        return this;
+    }
 }
